19a9f74ca9e7b4c5ad7d5a18d98a0b6f27648377
Peyton Doyle <peyton.doyle@icloud.com>
2025-05-25 10:22:08 -0400

    changed favicon and thumbnail

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta property="og:title" content="WALLPEYPERS" />
+        <meta property="og:description" content="Download dreamy seasonal wallpapers 🌅" />
+        <meta property="og:image" content="https://wallpeypers.vercel.app/WALLPEYPERS.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
